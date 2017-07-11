4f091309c91930b15cce8ca5feb91bc45ae01aa2
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-07-11 20:20:51 +0100

    Remove conversations and messages from schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,25 +15,6 @@ ActiveRecord::Schema.define(version: 20170711184352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "conversations", force: :cascade do |t|
-    t.integer "recipient_id"
-    t.integer "sender_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
-    t.index ["sender_id"], name: "index_conversations_on_sender_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id"
-    t.bigint "conversation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,7 +31,4 @@ ActiveRecord::Schema.define(version: 20170711184352) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
 end
