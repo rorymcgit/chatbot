7cac494196e9d08fd2ae0b66372882c8e48ac0e6
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-07-12 18:46:16 +0100

    Populate dropdown with answers

--- app/assets/javascripts/conversation.js
+++ app/assets/javascripts/conversation.js
@@ -2,11 +2,11 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
   $.getJSON("questions.json", function(data) {
-    var messageSelectForm = $("#message-select");
+    var messageSelectForm = $("#first-message-select");
 
     addGreetingToResponseDropDown(data);
 
-    $("#message-select").submit(function(e) {
+    $("#first-message-select").submit(function(e) {
       e.preventDefault();
       populateChatWithSelectedResponse();
       populateChatWithBotQuestion(data);
@@ -24,11 +24,23 @@ function addGreetingToResponseDropDown(data) {
 function populateChatWithSelectedResponse() {
   var selectedResponse = $("#responses").val();
   $(".chat_list").append($("<li class='human_msg'>" + selectedResponse + "</li>"));
-  $("#message-select").empty();
+  $(".chat_bottom").empty();
 }
 
 function populateChatWithBotQuestion(data) {
-  setTimeout(function(){
+  setTimeout(function() {
     $(".chat_list").append($("<li class='bot_msg'>" + data.questionTree.question + "</li>"));
+    addAnswerstoResponseDropdown(data)
   }, 1500);
+}
+
+function addAnswerstoResponseDropdown(data) {
+  // WiP. No submit button/form handler
+  $(".chat_bottom").append("<form id='answer-select'><select id='responses'></select></form")
+  var questionOneAnswers = data.questionTree.answers
+  for (var i = 0; i < questionOneAnswers.length; i++) {
+    $("#responses").append($("<option />")
+                   .val(questionOneAnswers[i])
+                   .text(questionOneAnswers[i]));
+  }
 }
