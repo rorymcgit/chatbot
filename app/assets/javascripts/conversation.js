4ca8056660d9d7dcf5d7707f14a4a2ed09ea5d20
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-07-12 17:46:38 +0100

    Refactor conversation.js into separate methods

--- app/assets/javascripts/conversation.js
+++ app/assets/javascripts/conversation.js
@@ -2,16 +2,33 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
   $.getJSON("questions.json", function(data) {
-    console.log(data);
-    $("#responses").append($("<option />")
-                   .val(data.greeting)
-                   .text(data.greeting)
-                   .attr('id','selected'));
-  });
+    var messageSelectForm = $("#message-select");
 
-  $("#message-select").submit(function(e) {
-    e.preventDefault();
-    var selectedResponse = $("#responses").val();
-    $(".chat_list").append($("<li class='chat_msg'>" + selectedResponse + "</li>"));
+    addGreetingToResponseDropDown(data);
+
+    $("#message-select").submit(function(e) {
+      e.preventDefault();
+      populateChatWithSelectedResponse();
+      populateChatWithBotQuestion(data);
+    });
   });
 });
+
+function addGreetingToResponseDropDown(data) {
+  $("#responses").append($("<option />")
+                 .val(data.greeting)
+                 .text(data.greeting)
+                 .attr('id','selected'));
+}
+
+function populateChatWithSelectedResponse() {
+  var selectedResponse = $("#responses").val();
+  $(".chat_list").append($("<li class='human_msg'>" + selectedResponse + "</li>"));
+  $("#message-select").empty();
+}
+
+function populateChatWithBotQuestion(data) {
+  setTimeout(function(){
+    $(".chat_list").append($("<li class='bot_msg'>" + data.questionTree.question + "</li>"));
+  }, 1500);
+}
